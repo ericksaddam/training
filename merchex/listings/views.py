353e02ddbd70efce18f9fed025b3af91aae49519
Erick Saddam <ericksaddam3@outlook.com>
2022-08-31 12:55:41 +0300

    Models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,24 @@
+from optparse import TitledHelpFormatter
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Band, Title
+
 def hello(request):
-    return HttpResponse("<h1>Hello Django!<h1/></b> <a href='/listings'>Visit Listings </a>")
+    bands =Band.objects.all()
+    title = Title.objects.all()
+    return HttpResponse(f"""<h1>Hello Django!<h1/></b> <a href='/listings'>Visit Listings </a></b>
+    <p>My favorurite bands:</p></b>
+    <ul>
+        <li>{bands[0].name}</li>
+        <li>{bands[1].name}</li>
+        <li>{bands[2].name}</li></b>
+        <p>My favourite titles:</p>
+        <li>{title[0].name}</li>
+        <li>{title[1].name}</li>
+        <li>{title[2].name}</li>
+    </ul>""")
 
 def about(request):
     return HttpResponse("<h1>About Us!<h1/></b> <a href='/contact'>Visit Contact </a>")
