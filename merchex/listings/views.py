aa6f20a3e62714ed3005f5e5c7975d70053d88e5
Erick Saddam <ericksaddam3@outlook.com>
2022-09-27 12:41:36 +0300

    render request

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -8,23 +8,14 @@ from listings.models import Band, Title
 def hello(request):
     bands =Band.objects.all()
     title = Title.objects.all()
-    return HttpResponse(f"""<h1>Hello Django!<h1/></b> <a href='/listings'>Visit Listings </a></b>
-    <p>My favorurite bands:</p></b>
-    <ul>
-        <li>{bands[0].name}</li>
-        <li>{bands[1].name}</li>
-        <li>{bands[2].name}</li></b>
-        <p>My favourite titles:</p>
-        <li>{title[0].name}</li>
-        <li>{title[1].name}</li>
-        <li>{title[2].name}</li>
-    </ul>""")
+    return render(request, 'listings/hello.html', {'Title': title,'bands':bands})
 
 def about(request):
-    return HttpResponse("<h1>About Us!<h1/></b> <a href='/contact'>Visit Contact </a>")
-
+    bands = Band.objects.all()
+    return render(request, 'listings/about.html')
+    
 def contact(request):
-    return HttpResponse("<h1>Contact page</h1></b> <a href='/hello'>Visit Home </a>")
+    return render(request, 'listings/contact.html')
 
 def listings(request):
-    return HttpResponse("<h1>Hello Lists!<h1/></b> <a href='/about-us'>Visit About us </a>")
+    return render(request, 'listings/listings.html')
